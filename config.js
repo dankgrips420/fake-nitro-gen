c7c905403f2b24ba5f504b44356f35e16d3cfba3
Androz2091 <androz2091@gmail.com>
2019-12-10 20:45:02 +0100

    Init repository :tada:

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    // The token of the bot
+    token: "",
+    // The ID of the channel in which the users will be able to ask for their fake classic nitro
+    genClassic: "",
+    // The ID of the channel in which the users will be able to ask for their fake games nitro
+    genGames: "",
+    // The prefix of the bot
+    prefix: "+"
+}
